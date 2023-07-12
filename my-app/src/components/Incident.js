cb8180dc01903d772b7f6d09dafac68f6766a544
Elisabet Asta <eao13@hi.is>
2023-07-12 11:28:44 +0000

    eftir að laga dags

--- my-app/src/components/Incident.js
+++ my-app/src/components/Incident.js
@@ -2,38 +2,40 @@ import React from 'react';
 import { View, Text, StyleSheet, Platform } from 'react-native';
 
 const Incident = ({ data }) => {
+  const createdDate = new Date(data.created);
+  const formattedDate = `${createdDate.toLocaleDateString()} - ${createdDate.toLocaleTimeString()}`;
+
   return (
     <View style={styles.card}>
-      <Text accessibilityLabel="title" style={styles.titleText}>{data.title}</Text>
+      <Text accessibilityLabel="title" style={styles.titleText}>Title: {data.title}</Text>
       <Text accessibilityLabel="description">Description: {data.description}</Text>
       <Text accessibilityLabel="feedback">Feedback: {data.feedback}</Text>
-      <Text accessibilityLabel="created">Created: {data.created}</Text>
+      <Text accessibilityLabel="created">Created: {formattedDate}</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    card: {
-      backgroundColor: '#fff',
-      borderRadius: 8,
-      margin: 8,
-      padding: 16,
-      ...Platform.select({
-        android: {
-          elevation: 4, // Add shadow on Android
-        },
-        ios: {
-          shadowColor: '#000',
-          shadowOffset: { width: 0, height: 2 },
-          shadowOpacity: 0.3,
-          shadowRadius: 4,
-        },
-      }),
-    },
-    titleText: {
-      fontWeight: 'bold',
-    },
-  });
-  
+  card: {
+    backgroundColor: '#fff',
+    borderRadius: 8,
+    margin: 8,
+    padding: 16,
+    ...Platform.select({
+      android: {
+        elevation: 4, // Add shadow on Android
+      },
+      ios: {
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.3,
+        shadowRadius: 4,
+      },
+    }),
+  },
+  titleText: {
+    fontWeight: 'bold',
+  },
+});
 
 export default Incident;
