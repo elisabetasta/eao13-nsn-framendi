edbf602ab13310aeb5ac89ed90feb06ed0c9adf0
Elisabet Asta <eao13@hi.is>
2023-07-12 14:55:28 +0000

    dagur check

--- my-app/src/components/Incident.js
+++ my-app/src/components/Incident.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { View, Text, StyleSheet, Platform } from 'react-native';
+import { format } from 'date-fns';
 
 const Incident = ({ data }) => {
   const createdDate = new Date(data.created);
-  const formattedDate = `${createdDate.toLocaleDateString()} - ${createdDate.toLocaleTimeString()}`;
+  const formattedDate = format(createdDate, 'dd.MM.yyyy');
+  const formattedTime = createdDate.toLocaleTimeString([], { hour12: false }).slice(0, 5);
 
   return (
     <View style={styles.card}>
       <Text accessibilityLabel="title" style={styles.titleText}>Title: {data.title}</Text>
       <Text accessibilityLabel="description">Description: {data.description}</Text>
       <Text accessibilityLabel="feedback">Feedback: {data.feedback}</Text>
-      <Text accessibilityLabel="created">Created: {formattedDate}</Text>
+      <Text accessibilityLabel="created">Created: {formattedDate} - {formattedTime}</Text>
     </View>
   );
 };
