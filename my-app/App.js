b6b1b2d5a3dbec6124d3320af098563543ff5a92
Elisabet Asta <eao13@hi.is>
2023-07-14 14:57:22 +0000

    refresh fastur

--- my-app/App.js
+++ my-app/App.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'intl-pluralrules';
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import IncidentList from './src/components/IncidentList';
-import './src/i18n'
-
+import { ButtonComponent } from './src/components/ButtonComponent';
+import './src/i18n';
 
 export default function App() {
+  const [refreshFlag, setRefreshFlag] = useState(false);
+
+  const handleRefresh = () => {
+    setRefreshFlag((prevFlag) => !prevFlag);
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Text>Header Component</Text>
       </View>
-      <ScrollView style={styles.scrollContainer}>
-        <IncidentList />
-      </ScrollView>
+      <View style={styles.contentContainer}>
+        <ButtonComponent onRefresh={handleRefresh} />
+        <ScrollView style={styles.scrollContainer}>
+          <IncidentList refreshFlag={refreshFlag} />
+        </ScrollView>
+      </View>
       <View style={styles.footer}>
         <Text>Footer Component</Text>
       </View>
@@ -31,6 +40,9 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#ddd',
+  },
+  contentContainer: {
+    flex: 1,
   },
   scrollContainer: {
     flex: 1,
--- my-app/src/components/IncidentList.js
+++ my-app/src/components/IncidentList.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { fetchData } from '../api/api';
 import Incident from './Incident';
-import {ButtonComponent} from './buttonComponent'
+import { ButtonComponent } from './ButtonComponent';
 
-const IncidentList = () => {
+const IncidentList = ({ refreshFlag }) => {
   const [incidentData, setIncidentData] = useState([]);
 
   useEffect(() => {
@@ -18,25 +18,45 @@ const IncidentList = () => {
     };
 
     fetchIncidentData();
-  }, []);
+  }, [refreshFlag]);
 
   const handleRefresh = async () => {
     try {
-        const data = await fetchData();
-        setIncidentData(data);
+      const data = await fetchData();
+      setIncidentData(data);
     } catch (error) {
-        console.error(error);
+      console.error(error);
     }
   };
 
   return (
-    <View>
-        <ButtonComponent onRefresh={handleRefresh}/>
-      {incidentData.map((incident) => (
-        <Incident key={incident.id} data={incident} />
-      ))}
+    <View style={styles.container}>
+      {/* <View style={styles.buttonContainer}> */}
+        {/* <ButtonComponent onRefresh={handleRefresh} /> */}
+      {/* </View> */}
+      <View style={styles.incidentContainer}>
+        {incidentData.map((incident) => (
+          <Incident key={incident.id} data={incident} />
+        ))}
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    height: '100%',
+  },
+  buttonContainer: {
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f1f1f1',
+  },
+  incidentContainer: {
+    flex: 1,
+  },
+});
 
 export default IncidentList;
