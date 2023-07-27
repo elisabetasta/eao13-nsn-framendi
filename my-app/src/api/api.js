2c7dfe5574079f9751562a9957b9fbd87c70f40d
Elisabet Asta <eao13@hi.is>
2023-07-27 11:30:33 +0000

    local db

--- my-app/src/api/api.js
+++ my-app/src/api/api.js
@@ -1,29 +1,17 @@
-import { NativeModules } from 'react-native';
-const { PostgreSQL } = NativeModules;
-
-const API_URL = process.env.EXPO_PUBLIC_BACKEND_URL;
-
+import axios from 'axios';
+const API_URL = 'http://localhost:3701/incidents'
 // Define a function to fetch the data
 const fetchData = async () => {
   try {
-    // Establish a connection to the PostgreSQL database
-    const client = new PostgreSQL(API_URL);
-    await client.connect();
-
-    // Execute your query (replace 'your_table' with your actual table name)
-    const res = await client.query('SELECT * FROM incident');
-    const data = res.rows;
-
-    // Close the connection
-    await client.end();
-
-    // Process the received data
-    return data;
-  } catch (error) {
-    console.log("lína 23 í api.js")
-    // Handle any errors
+     const response = await axios.get(API_URL);
+     const data = response.data;
+     // Process the received data
+     // console.log("fetchData() ", data)
+     return data;
+   } catch (error) {
+     // Handle any errors
     throw error;
   }
-};
+ };
 
-export { fetchData };
+ export { fetchData };
